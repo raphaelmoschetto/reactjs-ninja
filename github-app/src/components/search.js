ee7e96f3e9cd23d346bfc78ab7a674d06d09185b
Arnaldo Raphael Moschetto <consultor5a.moschetto@webmotors.com.br>
2018-08-10 13:06:00 -0300

    ReactJS Ninja

---
+++ github-app/src/components/search.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import React, { PropTypes } from 'react'
+
+const Search = ({ handleSearch, isDisabled }) => (
+  <div className='search'>
+    <input
+      type='search'
+      placeholder='Digite o nome do usuário no GitHub'
+      disabled={isDisabled}
+      onKeyUp={handleSearch} />
+  </div>
+)
+
+Search.propTypes = {
+  handleSearch: PropTypes.func.isRequired,
+  isDisabled: PropTypes.bool.isRequired
+}
+
+export default Search
