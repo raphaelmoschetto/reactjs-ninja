1039424041bcbecb9b4fb0796b219460f86cf3c9
Arnaldo Raphael Moschetto <consultor5a.moschetto@webmotors.com.br>
2018-08-13 19:05:59 -0300

    Assets Webpack

--- github-app/webpack.config.js
+++ github-app/webpack.config.js
@@ -3,6 +3,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const validate = require('webpack-validator')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = validate({
     devtool: 'source-map',
@@ -14,11 +15,15 @@ module.exports = validate({
     ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/dist/'
+        filename: '[name]-[hash].js',
+        publicPath: ''
     },
     plugins:[
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'GitHub App',
+            template: path.join(__dirname, 'src', 'html', 'template.html')
+        })
     ],
     module: {
         preLoaders: [{
@@ -32,6 +37,11 @@ module.exports = validate({
             exclude: /node_modules/,
             include: /src/,
             loader: 'babel'
+        }, {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            include: /src/,
+            loaders: ['style', 'raw']
         }]
     }
 })
